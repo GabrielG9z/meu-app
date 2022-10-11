2b91ad421c024125f388dcc92cb56394de299829
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-10-11 08:52:19 -0300

    conflitos css

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,10 +1,12 @@
 import Artigo from "../Artigo";
+import "./Conteudo.css";
 
 const Conteudo = () => {
   return (
     <>
       <main>
         <section>
+          <h2 className="subtitulo">Conteúdo do site</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Non porro
             maxime dolores laudantium illo commodi et excepturi accusamus odit

da88fc652e77a9951f6d9af88514d21e9beaa90e
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-10-11 11:59:55 -0300

    update
    bootstrap com react sendo aplicado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Cabecalho from "./components/layout/Cabecalho";
 import Conteudo from "./components/layout/Conteudo";
 import Footer from "./components/layout/Footer";
 import Artigo from "./components/Artigo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,9 +1,11 @@
 import Menu from "../Menu";
+import Button from "react-bootstrap/Button";
 const Cabecalho = () => {
   return (
     <>
       <header>
         <h1>Interface React</h1>
+        <Button variant="primary">Primary</Button>
       </header>
 
       <Menu />

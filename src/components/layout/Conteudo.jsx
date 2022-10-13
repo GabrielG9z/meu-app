0ed5247c827373b731c78fceda479e932c32e4a8
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-10-13 10:01:41 -0300

    update
    Adição de id e prop key , para que o map interprete o array certo.

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -3,18 +3,21 @@ import Artigo from "../Artigo";
 const Conteudo = () => {
   const artigos = [
     {
+      id: 1,
       titulo: "Artigo 1",
       subtitulo: "Este é subtitulo do artigo 1",
       texto: "Este é o conteúdo do artigo 1",
       curso: "JS Avançado",
     },
     {
+      id: 2,
       titulo: "Artigo 2",
       subtitulo: "Este é subtitulo do artigo 2",
       texto: "Este é o conteúdo do artigo 2",
       curso: "React",
     },
     {
+      id: 3,
       titulo: "Artigo 3",
       subtitulo: "Este é subtitulo do artigo 3",
       texto: "Este é o conteúdo do artigo 3",
@@ -22,6 +25,7 @@ const Conteudo = () => {
     },
   ];
 
+  /* Versão refatorada */
   return (
     <>
       <main>
@@ -29,17 +33,16 @@ const Conteudo = () => {
           <h2>Conteúdo do site...</h2>
           <p></p>
 
-          {artigos.map((artigo) => {
-            return (
-              <Artigo
-                titulo={artigo.titulo}
-                subtitulo={artigo.subtitulo}
-                curso={artigo.curso}
-              >
-                {artigo.texto}
-              </Artigo>
-            );
-          })}
+          {artigos.map((artigo) => (
+            <Artigo
+              key={artigo.id}
+              titulo={artigo.titulo}
+              subtitulo={artigo.subtitulo}
+              curso={artigo.curso}
+            >
+              {artigo.texto}
+            </Artigo>
+          ))}
         </section>
       </main>
     </>
